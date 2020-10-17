999a2ed01d920f4a77be5e35a6c0fe37ed09e8a6
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-10-17 01:06:41 -0400

    express setup continue

--- express/authapp/server.js
+++ express/authapp/server.js
@@ -1,10 +1,12 @@
-requestAnimationFrame("dotenv").config();
+require ("dotenv").config();
 const express = require("express");
 const app = express();
 const {PORT = 3000} = process.env;
 const cors = require ("cors");
 const morgan = require ("morgan");
 const mongoose = require ("./db/db");
+const AuthRouter = require("./controllers/user")
+const auth = require("./auth")
 
 //Middleware
 
@@ -13,6 +15,11 @@ app.use(cors());
 app.use(morgan("tiny"))
 
 //Routers
+app.get("/", auth, (req, res) => {
+    res.json(req.payload)
+});
+
+app.use("/auth", AuthRouter)
 
 //Listeners
 
